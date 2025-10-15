b25270d413336b602539381abe8d44da6a7841e8
Rommchy <rlashkevich1@gmail.com>
2025-10-15 22:11:18 +0300

    working on js

--- js/index.header-hero.js
+++ js/index.header-hero.js
@@ -1,9 +1,17 @@
 const headerBurger = document.querySelector(".header__burger");
 const burgerLines = document.querySelectorAll(".header__burger-line");
 const headerMobileMenu = document.querySelector(".header__mobile-menu");
+const headerMenuLink = document.querySelectorAll(".header__menu-link");
 
 headerBurger.addEventListener("click", () => {
   // console.log("Бургер натиснуто!");
   headerBurger.classList.toggle("show");
   headerMobileMenu.classList.toggle("show");
+  document.body.classList.toggle("no-scroll");
+});
+
+headerMenuLink.addEventListener("click", () => {
+  headerBurger.classList.toggle("show");
+  headerMobileMenu.classList.toggle("show");
+  document.body.classList.toggle("no-scroll");
 });
